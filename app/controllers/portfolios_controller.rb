aa473bfb9770fa6e62fa42783bd06c919fcbffe5
GproSoftware828 <JetSendProductions@Gmail.com>
2018-02-19 19:06:08 -0600

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,22 +1,40 @@
 class PortfoliosController < ApplicationController
 	def index
-		@portfolio_items = Portfolio.all
+		@portfolio_item = Portfolio.all
 	end
 
 	def new
-			@portfolio_items = Portfolio.new
+			@portfolio_item = Portfolio.new
 	end
 
 	  def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if @portfolio_items.save
+      if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @portfolio_items }
+        format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+    def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
   end
