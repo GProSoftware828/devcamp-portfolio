a7bba1f80d62ebfe06f0a1333c509c7b3d5af601
GproSoftware828 <JetSendProductions@Gmail.com>
2018-02-24 18:58:00 -0600

    Implemented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,11 +3,15 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.all
 	end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
 	def new
-			@portfolio_item = Portfolio.new
+		@portfolio_item = Portfolio.new
 	end
 
-	  def create
+	def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
