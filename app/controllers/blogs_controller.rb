bbeb38d9c2ce46265a3d47d82fdc1ce01f08ef10
GproSoftware828 <JetSendProductions@Gmail.com>
2018-04-01 19:29:58 -0500

    Updated several files to fix new blog errors from routes and active record, still working on it

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,41 +1,40 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
 
   # GET /blogs
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blogs = Blog.find(params[:id])
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
   def new
-    @blogs = Blog.new
+    @blog = Blog.new
   end
 
   # GET /blogs/1/edit
   def edit
-    @blogs = Blog.find(params[:id])
   end
 
   # POST /blogs
   # POST /blogs.json
   def create
-    @blog = Blog.new(params.require(:blog).permit(:title, :body))
+    @blog = Blog.new(blog_params)
 
     respond_to do |format|
-      if @blogs.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -46,10 +45,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -59,9 +56,19 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
