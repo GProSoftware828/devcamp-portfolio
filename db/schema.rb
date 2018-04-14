80ecc8631c1ab12b23ea87e2b74bb735dafe0112
GproSoftware828 <JetSendProductions@Gmail.com>
2018-04-14 03:35:40 -0500

    Added installation requirements for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308034313) do
+ActiveRecord::Schema.define(version: 20180414083229) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20180308034313) do
     t.string "slug"
     t.integer "status", default: 0
     t.integer "topic_id"
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
-    t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20180308034313) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
