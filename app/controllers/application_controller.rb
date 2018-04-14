8cb007679cf8b000282f6cc3832731bad515b6a4
GproSoftware828 <JetSendProductions@Gmail.com>
2018-04-14 03:27:00 -0500

    Initial gem building push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,3 +6,17 @@ class ApplicationController < ActionController::Base
   include DefaultPageContent
 
 end
+
+before_action :set_copyright
+
+def set_copyright
+	@copyright = DevcampViewTool::Renderer.copyright 'Georgie Payne', 'All rights reserved'
+end
+
+module DevcampViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
+end
