28034f077503bb8ac6c7c41f9e8db0f8097066e9
GproSoftware828 <JetSendProductions@Gmail.com>
2018-05-24 19:38:34 -0500

    Fixing errors

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,18 +1,18 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.special_blogs
     @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blogs = Blog.find(params[:id])
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
