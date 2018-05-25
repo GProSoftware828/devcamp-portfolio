b58ccf1811d6775be44ab3d48d41d0f8a3eb2577
GproSoftware828 <JetSendProductions@Gmail.com>
2018-05-24 19:59:39 -0500

    Application controller not prepared from rubygem branch checkout to fix bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
-end
-
-before_action :set_copyright
-
-def set_copyright
-	@copyright = DevcampViewTool::Renderer.copyright 'Georgie Payne', 'All rights reserved'
-end
-
-module DevcampViewTool
-	class Renderer
-		def self.copyright name, msg
-			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-		end
-	end
 end
