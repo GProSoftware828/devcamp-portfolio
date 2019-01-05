e6fd2fc1618297785eff2ef2054a9d789bbdb7bc
GproSoftware828 <JetSendProductions@Gmail.com>
2019-01-05 23:39:26 +1100

    Fixed stylizing bugs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,7 @@
 class PagesController < ApplicationController
-  def home
-  	@posts = Blog.all
-  	@skills = Skill.all
-  end
-
+  
   def about
     @skills = Skill.all
-  end
-
-  def contact
   end
 
 end
