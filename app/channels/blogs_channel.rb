b81e24d9e424f942d93b334f4c90500f962f04b5
GproSoftware828 <JetSendProductions@Gmail.com>
2019-01-06 21:10:54 +1100

    Refactor

--- app/channels/blogs_channel.rb
+++ app/channels/blogs_channel.rb
@@ -1,4 +1,4 @@
-class BlogsChannel < ApplicationCable::BlogsChannel
+class BlogsChannel < ApplicationCable::Channel
   #  #the show div passes the comment model (ass. to blogs and users) into Action Cable's channel component and uses its stream method to make the channel component dynamic:
   def subscribed
     stream_from "blogs_#{params['blog_id']}_channel"
